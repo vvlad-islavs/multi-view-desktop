2a29cca597dbf03a9d1249729c3aeb00245137da
Vlad Islav <islavvvlad@gmail.com>
2026-07-03 14:11:38 +0200

    ref: calc_window_position refactor

--- lib/src/utils/calc_window_position.dart
+++ lib/src/utils/calc_window_position.dart
@@ -5,73 +5,32 @@ import 'package:flutter/material.dart';
 
 import '../screen_retriever/screen_retriever.dart';
 
+/// macOS title-bar inset. Without it parent-relative dialogs sit too low.
+const int _macTopRectInset = 38;
+
 @internal
 Future<Offset> calcWindowPosition(Size windowSize, Alignment alignment) async {
-  final screenRetriever = ScreenRetriever.instance;
-  final Display primaryDisplay = await screenRetriever.getPrimaryDisplay();
-  final List<Display> allDisplays = await screenRetriever.getAllDisplays();
-  final Offset cursorScreenPoint = await screenRetriever.getCursorScreenPoint();
-
-  final Display currentDisplay = allDisplays.firstWhere(
-    (display) => Rect.fromLTWH(
-      display.visiblePosition?.dx ?? 0,
-      display.visiblePosition?.dy ?? 0,
-      display.size.width,
-      display.size.height,
-    ).contains(cursorScreenPoint),
-    orElse: () => primaryDisplay,
-  );
-
+  final currentDisplay = await _getCurrentDisplay();
   final num visibleWidth = currentDisplay.visibleSize?.width ?? currentDisplay.size.width;
   final num visibleHeight = currentDisplay.visibleSize?.height ?? currentDisplay.size.height;
   final num visibleStartX = currentDisplay.visiblePosition?.dx ?? 0;
   final num visibleStartY = currentDisplay.visiblePosition?.dy ?? 0;
 
-  final Offset position;
-
-  Offset forDefault() {
-    final left = (visibleWidth - windowSize.width) / 2 + alignment.x * ((visibleWidth - windowSize.width) / 2);
-    final top = (visibleHeight - windowSize.height) / 2 + alignment.y * ((visibleHeight - windowSize.height) / 2);
-    return Offset(visibleStartX + left, visibleStartY + top);
-  }
-
-  position = switch (alignment) {
-    Alignment.topLeft => Offset(visibleStartX.toDouble(), visibleStartY.toDouble()),
-    Alignment.topCenter => Offset(
-      visibleStartX + (visibleWidth / 2) - (windowSize.width / 2),
-      visibleStartY.toDouble(),
-    ),
-    Alignment.topRight => Offset(visibleStartX + visibleWidth - windowSize.width, visibleStartY.toDouble()),
-    Alignment.centerLeft => Offset(
-      visibleStartX.toDouble(),
-      visibleStartY + (visibleHeight / 2) - (windowSize.height / 2),
-    ),
-    Alignment.center => Offset(
-      visibleStartX + (visibleWidth / 2) - (windowSize.width / 2),
-      visibleStartY + (visibleHeight / 2) - (windowSize.height / 2),
-    ),
-    Alignment.centerRight => Offset(
-      visibleStartX + visibleWidth - windowSize.width,
-      visibleStartY + (visibleHeight / 2) - (windowSize.height / 2),
-    ),
-    Alignment.bottomLeft => Offset(visibleStartX.toDouble(), visibleStartY + visibleHeight - windowSize.height),
-    Alignment.bottomCenter => Offset(
-      visibleStartX + (visibleWidth / 2) - (windowSize.width / 2),
-      visibleStartY + visibleHeight - windowSize.height,
-    ),
-    Alignment.bottomRight => Offset(
-      visibleStartX + visibleWidth - windowSize.width,
-      visibleStartY + visibleHeight - windowSize.height,
-    ),
-    _ => forDefault(),
-  };
+  final Offset position = calcPosition(
+    alignment: alignment,
+    windowSize: windowSize,
+    visibleWidth: visibleWidth,
+    visibleHeight: visibleHeight,
+    visibleStartX: visibleStartX,
+    visibleStartY: visibleStartY,
+  );
 
   return position;
 }
 
 int get _platformTopRectAddSize {
   if (Platform.isMacOS) {
-    return 38;
+    return _macTopRectInset;
   }
   return 0;
 }
@@ -82,35 +41,42 @@ Future<Offset> calcWindowPositionByParent(
   required Size windowSize,
   required Rect parentBounds,
 }) async {
-  final screenRetriever = ScreenRetriever.instance;
-  final Display primaryDisplay = await screenRetriever.getPrimaryDisplay();
-  final List<Display> allDisplays = await screenRetriever.getAllDisplays();
-  final Offset cursorScreenPoint = await screenRetriever.getCursorScreenPoint();
-
-  final Display currentDisplay = allDisplays.firstWhere(
-    (display) => Rect.fromLTWH(
-      display.visiblePosition?.dx ?? 0,
-      display.visiblePosition?.dy ?? 0,
-      display.size.width,
-      display.size.height,
-    ).contains(cursorScreenPoint),
-    orElse: () => primaryDisplay,
-  );
+  final currentDisplay = await _getCurrentDisplay();
 
   final num visibleWidth = parentBounds.size.width;
   final num visibleHeight = parentBounds.size.height;
   final num visibleStartX = (currentDisplay.visiblePosition?.dx ?? 0) + parentBounds.left;
   final num visibleStartY = (currentDisplay.visiblePosition?.dy ?? 0) + parentBounds.top - _platformTopRectAddSize;
 
-  final Offset position;
+  final Offset position = calcPosition(
+    alignment: alignment,
+    windowSize: windowSize,
+    visibleWidth: visibleWidth,
+    visibleHeight: visibleHeight,
+    visibleStartX: visibleStartX,
+    visibleStartY: visibleStartY,
+  );
 
+  return position;
+}
+
+@internal
+@visibleForTesting
+Offset calcPosition({
+  required Alignment alignment,
+  required Size windowSize,
+  required num visibleWidth,
+  required num visibleHeight,
+  required num visibleStartX,
+  required num visibleStartY,
+}) {
   Offset forDefault() {
     final left = (visibleWidth - windowSize.width) / 2 + alignment.x * ((visibleWidth - windowSize.width) / 2);
     final top = (visibleHeight - windowSize.height) / 2 + alignment.y * ((visibleHeight - windowSize.height) / 2);
     return Offset(visibleStartX + left, visibleStartY + top);
   }
 
-  position = switch (alignment) {
+  final position = switch (alignment) {
     Alignment.topLeft => Offset(visibleStartX.toDouble(), visibleStartY.toDouble()),
     Alignment.topCenter => Offset(
       visibleStartX + (visibleWidth / 2) - (windowSize.width / 2),
@@ -142,4 +108,21 @@ Future<Offset> calcWindowPositionByParent(
   };
 
   return position;
+}
+
+Future<Display> _getCurrentDisplay() async {
+  final screenRetriever = ScreenRetriever.instance;
+  final Display primaryDisplay = await screenRetriever.getPrimaryDisplay();
+  final List<Display> allDisplays = await screenRetriever.getAllDisplays();
+  final Offset cursorScreenPoint = await screenRetriever.getCursorScreenPoint();
+
+  return allDisplays.firstWhere(
+    (display) => Rect.fromLTWH(
+      display.visiblePosition?.dx ?? 0,
+      display.visiblePosition?.dy ?? 0,
+      display.size.width,
+      display.size.height,
+    ).contains(cursorScreenPoint),
+    orElse: () => primaryDisplay,
+  );
 }
